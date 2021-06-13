35ed90f04fbd767a98f5218afdcd4b443482b704
Cosmic <60071537+CosmicAdc@users.noreply.github.com>
2021-06-12 19:12:54 -0500

    Calculadora 12/6/2021

---
+++ Calculadora  12_6_21/src/calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+
+package calculadora;
+import pantalla.pantalla;
+public class Calculadora {
+
+    public static void main(String[] args) {
+        pantalla pantalla=new pantalla();
+        pantalla.setVisible (true);
+    }
+    
+}
