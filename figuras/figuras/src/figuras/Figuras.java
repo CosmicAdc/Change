bf556e0dfefcc2ff443f72f149c330de8edb2292
Cosmic <60071537+CosmicAdc@users.noreply.github.com>
2021-06-22 10:08:48 -0500

    Figuras 21_06_2021

---
+++ figuras/figuras/src/figuras/Figuras.java
@@ -0,0 +1,13 @@
+
+package figuras;
+import graficas.Ventana;
+public class Figuras  {
+
+   
+    public static void main(String[] args) {
+            Ventana ventana=new Ventana();
+            ventana.initComponentes();
+            ventana.setVisible(true);
+    }
+    
+}
