a8fa572a8f7c908f4d369247b63dbdc63a8ef479
Cosmic <60071537+CosmicAdc@users.noreply.github.com>
2021-06-22 21:03:20 -0500

    Juego 3 en raya

---
+++ tresRaya 22_06_2021/src/tresraya/TresRaya.java
@@ -0,0 +1,14 @@
+
+package tresraya;
+import graficas.Ventana;
+public class TresRaya {
+
+    public static void main(String[] args) {
+         Ventana ventana=new Ventana();
+
+         ventana.initComponentes();
+         ventana.setVisible(true);
+         
+    }
+    
+}
